feb3a95bf8b792708a179d67930e374143a254fc
Mottelz <shachna@gmail.com>
2019-12-10 21:06:13 -0500

    Updates

---
+++ precache-manifest.fff5d02f6ae7dcf02b1f054f383b2ad3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecaa37c6db49c4692b33d0462b24820a",
+    "url": "/MonsterCalculator/index.html"
+  },
+  {
+    "revision": "6f5c082592e5bc2efe40",
+    "url": "/MonsterCalculator/static/js/2.9503e013.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterCalculator/static/js/2.9503e013.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95f23f13e8bc3ba1b3e0",
+    "url": "/MonsterCalculator/static/js/main.ec50f05b.chunk.js"
+  },
+  {
+    "revision": "3a04503c883f549ad425",
+    "url": "/MonsterCalculator/static/js/runtime-main.60100da0.js"
+  }
+]);
